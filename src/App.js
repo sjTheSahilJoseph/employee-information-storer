6e88fe3ec3660888c6bbb649d52d1f843f1cc05e
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2023-09-29 22:22:00 +0500

    Basic Styling Stuff

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import Employee from "./components/Employee";
 
 function App() {
   return (
     <>
-
+    <div className="container flex flex-wrap justify-center mt-3">
+      <Employee name={'SJ the Sahil Joseph'} image={'https://source.unsplash.com/200x200/?man,office'} role={'Software Engineer'}/>
+      <Employee name={'SJ the Sahil Joseph'} image={'https://source.unsplash.com/200x200/?man,office'} role={'Software Engineer'}/>
+      <Employee name={'SJ the Sahil Joseph'} image={'https://source.unsplash.com/200x200/?man,office'} role={'Software Engineer'}/>
+      <Employee name={'SJ the Sahil Joseph'} image={'https://source.unsplash.com/200x200/?man,office'} role={'Software Engineer'}/>
+      <Employee name={'SJ the Sahil Joseph'} image={'https://source.unsplash.com/200x200/?man,office'} role={'Software Engineer'}/>
+      <Employee name={'SJ the Sahil Joseph'} image={'https://source.unsplash.com/200x200/?man,office'} role={'Software Engineer'}/>
+      <Employee name={'SJ the Sahil Joseph'} image={'https://source.unsplash.com/200x200/?man,office'} role={'Software Engineer'}/>
+      </div>
     </>
   );
 }
